6d70ce25f03e7a3235ffb920675aa3e4fbe3b124
junjunghasudungan <junjunghasudungan43@gmail.com>
2024-08-15 09:23:43 +0700

    constructor dengan variable parameter dan diberi nilai default

--- constructor/User.php
+++ constructor/User.php
@@ -6,18 +6,29 @@
         public $id, $name, $email, $address, $role;
 
         // melakukan pembuatan konstrutor menggunakan method spesial
-        public function __construct(  )
-        {
-            $this->id  = 2;
-            $this->name  = "Asep";
-            $this->email  = "user@example.com";
-            $this->address  = "Bandung";
-            $this->role  = "user";
+        // public function __construct(  ) // method construct tanpa variable parameter
+        // {
+        //     $this->id  = 2;
+        //     $this->name  = "Asep";
+        //     $this->email  = "user@example.com";
+        //     $this->address  = "Bandung";
+        //     $this->role  = "user";
+        // }
+
+        public function __construct(
+            $id = 2, $name = "test user", $email = "testUser@example.com", 
+                $address = "batam", $role = "user") {
+                $this->id  = $id;
+                $this->name  = $name;
+                $this->email  = $email;
+                $this->address  = $address;
+                $this->role  = $role;
         }
+
     }
 
     // melakukan instansiasi class-objek
-    $user1 = new User();
+    $user1 = new User("1", "Hasudungan", "admin@example.com", "Batam", "admin");
     $user2 = new User();
 
     // melakukan koleksi data user kedalam variable array
